44092c1dd1a91bdf21a24e1c62318e2ed5c36941
Alexandre Foulc <alexandre-foulc@orange.fr>
2024-11-05 09:58:00 +0100

    Templating initial

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -12,8 +12,6 @@ class ReservationsController extends AbstractController
   #[Route('/reservations', name: 'app_reservations')]
   public function index(): Response
   {
-    return $this->render('reservations/index.html.twig', [
-      'controller_name' => 'ReservationsController',
-    ]);
+    return $this->render('reservations/index.html.twig', []);
   }
 }
